f2e00831e7262d6f0b3b5da89aa0ba62cac199fd
Shinshu-University <154774167+Shinshu-University@users.noreply.github.com>
2025-03-16 08:36:43 +0900

    Update encryptPlus.js

--- encryptPlus.js
+++ encryptPlus.js
@@ -36,7 +36,12 @@
     async hash({ TEXT, ALGORITHM }) {
       const encoder = new TextEncoder();
       const data = encoder.encode(TEXT);
-      const hashBuffer = await crypto.subtle.digest(ALGORITHM, data);
+      let hashBuffer;
+      if (ALGORITHM === 'MD5') {
+        hashBuffer = await crypto.subtle.digest({ name: 'MD5' }, data); // MD5の場合
+      } else {
+        hashBuffer = await crypto.subtle.digest(ALGORITHM, data); // 他のアルゴリズムの場合
+      }
       const hashArray = Array.from(new Uint8Array(hashBuffer));
       const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
       return hashHex;
