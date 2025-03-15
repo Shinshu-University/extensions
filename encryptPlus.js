22cfaca80f07d2605564475909500cb15fbc9343
Shinshu-University <154774167+Shinshu-University@users.noreply.github.com>
2025-03-16 08:35:54 +0900

    Update and rename sha256.js to encryptPlus.js

---
+++ encryptPlus.js
@@ -0,0 +1,47 @@
+(function(Scratch) {
+  'use strict';
+
+  class HashExtension {
+    getInfo() {
+      return {
+        id: 'encryptPlus',
+        name: 'encrypt+',
+        blocks: [
+          {
+            opcode: 'hash',
+            blockType: Scratch.BlockType.REPORTER,
+            text: '[TEXT]を[ALGORITHM]でハッシュ化',
+            arguments: {
+              TEXT: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Hello, world!'
+              },
+              ALGORITHM: {
+                type: Scratch.ArgumentType.STRING,
+                menu: 'algorithms',
+                defaultValue: 'SHA-256'
+              }
+            }
+          }
+        ],
+        menus: {
+          algorithms: {
+            acceptReporters: true,
+            items: ['SHA-1', 'SHA-256', 'SHA-384', 'SHA-512', 'MD5']
+          }
+        }
+      };
+    }
+
+    async hash({ TEXT, ALGORITHM }) {
+      const encoder = new TextEncoder();
+      const data = encoder.encode(TEXT);
+      const hashBuffer = await crypto.subtle.digest(ALGORITHM, data);
+      const hashArray = Array.from(new Uint8Array(hashBuffer));
+      const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+      return hashHex;
+    }
+  }
+
+  Scratch.extensions.register(new HashExtension());
+})(Scratch);
